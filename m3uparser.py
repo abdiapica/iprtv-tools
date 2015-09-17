611a32220f6e7fa08ae359ef9d98a464532134ce
root <root@nuc2.enigma-x.nl>
2015-09-17 23:00:00 +0200

    added basic functionality

--- m3uparser.py
+++ m3uparser.py
@@ -1,5 +1,22 @@
-class m3uparser:
+class m3uParser:
 
     def __init__(self):
         self.data = []
+
+    def addItem(self,channelName,url):
+        item = {}
+        item['name'] = channelName
+        item['url'] = url
+
+        self.data.append(item)
+
+    def parseM3u(self):
+        
+        print "#EXTM3U"
+
+        for i in self.data:
+            print "#EXTINF:-1,",i['name']
+            print(i['url'])
+
+    #def writeM3u(filename):
 
