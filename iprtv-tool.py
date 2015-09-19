f6a58c94f6191c2508cb8319294caddbad97f39f
Tom Duijf <tom.duijf@gmail.com>
2015-09-19 12:15:46 +0200

    raw and yaml output format

--- iprtv-tool.py
+++ iprtv-tool.py
@@ -5,37 +5,41 @@ from pprint import pprint
 from tools import iprtv
 from tools import m3u
 
-# create an argparser
-parser = argparse.ArgumentParser()
-
-# add arguments to the parser
-parser.add_argument('--out', action='store', dest='parse_value', choices=['m3u', 'raw', 'udpxy'], default='m3u',
-                    help='Output format')
-
-parser.add_argument('--udpxy', action='store', dest='udpxy_value', 
-                    help='Use given udpxy url (i.e. http://192.168.0.1/4020)')
-
-# process all the arguments
-results = parser.parse_args()
-
-
 # do the magic here
 def main():
-    
-    # create a channelParser object
+    # create an argparser
+    parser = argparse.ArgumentParser()
+    # add arguments to the parser
+    parser.add_argument('--out', action='store', dest='out_format', choices=['m3u', 'raw', 'yaml', 'udpxy'], default='m3u', help='Output format')
+    parser.add_argument('--udpxy', action='store', dest='udpxy_prefix', help='Use given udpxy url (i.e. http://192.168.0.1/4020)')
+
+    # process all the arguments
+    results = parser.parse_args()
+
     channels = iprtv.getChannels( 'http://w.stb.zt6.nl/tvmenu/index.xhtml.gz' )
+    # create a channelParser object
     #pprint(iptvchannels)
+    if results.out_format == 'raw':
+        pprint( channels )
 
-    # create a m3uParser object
-    m3ulist = m3u.m3uParser()
+    elif results.out_format == 'yaml':
+        import yaml
+        print( yaml.dump( channels ) )
 
-    for c in channels:
-        streams = c['streams']
-        for s in streams:
-            m3ulist.addItem(c['name'],s['url'])
+    elif results.out_format == 'm3u':
+        # create a m3uParser object
+        m3ulist = m3u.m3uParser()
 
-    # parse whatever has to be parsed
-    m3ulist.parseM3u()
+        for c in channels:
+            streams = c['streams']
+            for s in streams:
+                m3ulist.addItem(c['name'],s['url'])
+
+        # parse whatever has to be parsed
+        m3ulist.parseM3u()
+
+    elif results.out_format == 'udpxy':
+        print( 'not done yet' )
 
 # allow this to be a module
 if __name__ == '__main__':
